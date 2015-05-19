063053edabeb1219da809cdf5024691486327953
renaud gaudin <rgaudin@gmail.com>
2015-05-19 14:48:44 +0000

    fixed launch of runserver and firefox FS

--- uninond/scripts/launch_ff.sh
+++ uninond/scripts/launch_ff.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # sleep first so fluxbox has time to load
-sleep 1
+# sleep 1
 
 # check for firefox process
 # launch firefox on first display
@@ -10,4 +10,4 @@ pgrep -f firefox || firefox --display :0
 sleep 3
 
 # emulate a touch on F11 to go fullscreen
-xdotool key F11
+DISPLAY=:0 xdotool key F11
