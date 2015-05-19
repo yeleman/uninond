0eaa8bbc037e755a498b874f772a3093812a726e
renaud gaudin <rgaudin@gmail.com>
2015-05-19 16:20:51 +0000

    fixed firefox closing on shutdown and FF launching in FS

--- uninond/scripts/launch_ff.sh
+++ uninond/scripts/launch_ff.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# sleep first so fluxbox has time to load
-# sleep 1
-
-# check for firefox process
-# launch firefox on first display
-pgrep -f firefox || LC_ALL=fr_FR.UTF-8 firefox --display :0 
-
-sleep 3
-
-# emulate a touch on F11 to go fullscreen
-DISPLAY=:0 xdotool key F11
+pgrep -f firefox
+if [ $? -ne 0 ];
+then
+	# launch firefox in french
+	# then emulate a touch on F11 to go fullscreen
+    LC_ALL=fr_FR.UTF-8 firefox --display :0 && sleep 3 && DISPLAY=:0 xdotool key F11
+fi
--- uninond/scripts/powerbtn.sh
+++ uninond/scripts/powerbtn.sh
@@ -10,8 +10,8 @@ else
 fi
 
 # close firefox nicely
-DISPLAY=:0 xdotool key alt+F4 && xdotool key alt+F4 && xdotool key alt+F4 && sleep 2
-su -l uninond -c kill -1 `pgrep -f firefox` && sleep 2
+DISPLAY=:0 sudo -u uninond xdotool key alt+F4 && sleep 1
+sudo -u uninond kill -1 `pgrep -f firefox` && sleep 1
 
 # Skip if we just in the middle of resuming.
 test -f /var/lock/acpisleep && exit 0
